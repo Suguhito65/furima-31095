d75237a454aba2781447dccf14dfb733d9669479
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-22 13:46:12 +0900

    ダウンロード素材を置換

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def cofigure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday]
+    )
+  end
 end
