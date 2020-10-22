0b941ad0047ad27aebb4a07a503225d05c13daf0
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-22 12:49:56 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+         
+  with_options presence: true do
+    validates :encrypted_password, format: { with: /\A[a-z\d]{6,100}+\z/, message: "Password Include both letters and numbers" }
+    validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters." }
+    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters." }
+    validates :last_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters." }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters." }
+  end
+         
+  validates :nickname,           presence: true
+  validates :last_name,          presence: true
+  validates :first_name,         presence: true
+  validates :last_name_reading,  presence: true
+  validates :first_name_reading, presence: true
+  validates :birthday,           presence: true
 end
