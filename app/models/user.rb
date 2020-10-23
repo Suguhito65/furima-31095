1186e72bca01f9617c6a3bb30f5976d707a874e4
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-23 15:45:01 +0900

    Userモデル単体テストコードの記述とバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,13 @@ class User < ApplicationRecord
   has_many :orders
          
   with_options presence: true do
-    validates :password,           format: { with: /\A[a-zA-Z0-9\d]+\z/, message: "Half-width alphanumeric" }
     validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters." }
     validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters." }
     validates :last_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters." }
     validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters." }
   end
-         
-  validates :nickname,           presence: true
-  validates :password,           presence: true, length: { minimum: 6 } #6文字以上
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_reading,  presence: true
-  validates :first_name_reading, presence: true
-  validates :birthday,           presence: true
+
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\d+\z/, message: "Include both letters and numbers" },length: { minimum: 6 }
+  validates :nickname, presence: true
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,11 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {Faker::Name.last_name}
-    first_name            {Faker::Name.first_name}
-    last_name_reading     {Faker::Name.last_name}
-    first_name_reading    {Faker::Name.first_name}
-    birthday              {Faker::Date}
+    password              {"aaaa11"}
+    last_name             {"田中"}
+    first_name            {"太郎"}
+    last_name_reading     {"タナカ"}
+    first_name_reading    {"タロウ"}
+    birthday              {Faker::Date.birthday}
   end
 end
