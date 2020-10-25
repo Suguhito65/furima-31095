6b505edffe7aa2d0081b4db98e2b718c89c5ff6e
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-25 17:15:39 +0900

    ActiveHashモデルのアソシエーションとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :delivery_days
   has_one :order
   belongs_to :user
   has_one_attached :image
+
+  validates :product, :description, :category, :condition, :burden, :area, :delivery_days, :price, presence:ture
+  validates :price, format: { with: [3-9][0-9][0-9], message: "Out of setting range" }
+  # id:0以外の時に保存できる
+  validates :category_id, :condition_id, :burden_id, :area_id, :delivery_days_id, numericality: { other_than: 0 }
 end
