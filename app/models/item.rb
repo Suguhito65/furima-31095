60c1bab2186326fb6f309ef8ec1d0dde5d62dfb5
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-25 18:42:02 +0900

    商品出品ページを表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :burden
   belongs_to_active_hash :area
-  belongs_to_active_hash :delivery_days
+  belongs_to_active_hash :DeliveryDay
   has_one :order
   belongs_to :user
   has_one_attached :image
 
-  validates :product, :description, :category, :condition, :burden, :area, :delivery_days, :price, presence:ture
-  validates :price, format: { with: [3-9][0-9][0-9], message: "Out of setting range" }
+  validates :product, :description, :category, :condition, :burden, :area, :delivery_days, :price, presence:true
+  # validates :price, format: { with: [3-9][0-9][0-9], message: "Out of setting range" }
   # id:0以外の時に保存できる
   validates :category_id, :condition_id, :burden_id, :area_id, :delivery_days_id, numericality: { other_than: 0 }
 end
