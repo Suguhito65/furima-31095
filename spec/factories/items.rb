8a2564566c1a440f8767c4f9d761caa707b4c8b9
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-26 17:18:30 +0900

    Itemモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    product        {Faker::Lorem.sentence}
+    description    {Faker::Lorem.sentence}
+    category_id    {'1'}
+    condition_id   {'1'}
+    burden_id      {'1'}
+    area_id        {'1'}
+    deliveryday_id {'1'}
+    price          {'2000'}
+    association :user
   end
 end
