2940311550e1f1346c804833921d89c821ac355d
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-28 15:29:35 +0900

    コメント削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :destroy] # except: [:index, :show, :new, :create]
+  before_action :move_to_index, only: [:edit, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
   end
