5c8aa2770ea8029969b4dc2b7c2b60c9db6fbdcf
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-28 14:31:39 +0900

    destroyアクション追加と諸々の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -34,6 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
