aaab3dbd948d86646346dee08668a98e8477fe12
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-29 14:42:13 +0900

    createアクションとストロングパラメーターの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.save
+    redirect_to action: :index
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number)
   end
 end
