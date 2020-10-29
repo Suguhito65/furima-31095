83d0d5a63ed86d8d0c9f5b90451413b2dd798f16
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-10-29 12:33:30 +0900

    Formオブジェクト作成、prefectureモデル作成とアソシエーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
+  belongs_to_active_hash :prefecture
 end
---
+++ app/models/purchase.rb
@@ -0,0 +1,3 @@
+class Purchase
+  include ActiveModel::Model
+end
